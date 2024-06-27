e1df44fff33e9056b4d3d69a20b2ddf84dd7b71a
xhh <1842435366@qq.com>
2024-06-27 15:56:16 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 执行命令 sh deploy.sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# push 到 github 项目的 gh-pages 分支
+git push -f https://github.com/org-xhh/xhh-blog.git master:gh-pages
+
+cd -
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# push 到 master
+git push -f https://github.com/org-xhh/xhh-blog.git master:master
--- docs/web/html/dom.md
+++ docs/web/html/dom.md
@@ -1,6 +1,6 @@
 
 # DOM 
-全称是 Document Object Model
+全称是 Document Object Model，文档对象模型
 
 
 https://developer.mozilla.org/zh-CN/docs/Web/API/Document
